edba18680cf36be70868f153e5cb7fe12dbe1283
Jonatan Cespedes <retratos.jonatan@gmail.com>
2021-07-23 11:04:28 -0300

    Clase 23/07 final

--- controllers/autosController.js
+++ controllers/autosController.js
@@ -13,6 +13,25 @@ let autosController = {
     },
     index: function(req, res) {
         res.send(autosController.autos())
+    },
+    marca: (req, res) => {
+        let marcaParams = req.params.marca.trim();
+
+        let filtradosPorMarca = [];
+
+        autosController.autos().forEach(auto => {
+            if(auto.marca.toLowerCase() === marcaParams.toLowerCase()){
+                filtradosPorMarca.push(auto)
+            }
+        })
+
+        res.send(filtradosPorMarca)
+    },
+    dato: (req, res) => {
+        let datoParams = req.params.dato;
+        switch (typeof datoParams) {
+            case Number:
+        }
     }
 }
 
--- routes/autos.js
+++ routes/autos.js
@@ -4,6 +4,8 @@ let router = express.Router();
 let controller = require('../controllers/autosController')
 
 router.get('/', controller.index);
+router.get('/:marca', controller.marca);
+router.get('/:marca/:dato?', controller.dato);
 
 
 module.exports = router;
